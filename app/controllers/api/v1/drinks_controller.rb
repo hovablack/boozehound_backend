7f1ed7dddb5b613d3b646cc81a7bdd7c339e04fd
Hova Black <nevermind92@hotmail.com>
2021-09-16 22:26:05 -0500

    Drink controller actions and routes

--- app/controllers/api/v1/drinks_controller.rb
+++ app/controllers/api/v1/drinks_controller.rb
@@ -1,2 +1,26 @@
 class Api::V1::DrinksController < ApplicationController
+
+  def index
+    drinks = Drink.all
+    render json: drinks
+  end
+  
+  def create
+    drink = Drink.new(drink_params)
+    if drink.save
+      render json: drink, status: :created, location: drink
+    else
+      render json: {errors: drink.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_drink
+    
+  end
+
+  def drink_params
+    params.require(:drink).permit(:name, :description, :image_url)
+  end
 end
